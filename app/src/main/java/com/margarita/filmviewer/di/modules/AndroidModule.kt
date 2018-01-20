0e73256d8564a1d05db31c920ca3f1ee2d651cf4
Margarita <margo.himera@yandex.ru>
2018-01-20 18:13:00 +0300

    Function for checking the Interntet connection was added

--- app/src/main/java/com/margarita/filmviewer/di/modules/AndroidModule.kt
+++ app/src/main/java/com/margarita/filmviewer/di/modules/AndroidModule.kt
@@ -12,11 +12,6 @@ import javax.inject.Singleton
 @Module
 class AndroidModule(private val application: Application) {
 
-    /**
-     * Allow the application context to be injected
-     * but require that it be annotated with [@Annotation][ForApplication]
-     * to explicitly differentiate it from an activity context.
-     */
-    @Provides @Singleton @ForApplication
+    @Provides @Singleton
     fun provideApplicationContext(): Context = application
 }
