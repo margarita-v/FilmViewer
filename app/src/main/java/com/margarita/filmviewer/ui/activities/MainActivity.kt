f28604a56c4748b646e89ca31972479ec25aba63
Margarita <margo.himera@yandex.ru>
2018-01-20 21:45:25 +0300

    Try to add MoviesFragment

--- app/src/main/java/com/margarita/filmviewer/ui/activities/MainActivity.kt
+++ app/src/main/java/com/margarita/filmviewer/ui/activities/MainActivity.kt
@@ -1,62 +1,32 @@
 package com.margarita.filmviewer.ui.activities
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import android.widget.EditText
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 import com.margarita.filmviewer.R
-import com.margarita.filmviewer.common.MovieAdapter
 import com.margarita.filmviewer.common.getColorResource
-import com.margarita.filmviewer.common.showSnackBar
-import com.margarita.filmviewer.models.Movie
-import com.margarita.filmviewer.mvp.presenter.MoviesPresenter
-import com.margarita.filmviewer.mvp.view.MoviesView
 import com.margarita.filmviewer.ui.fragments.EmptySearchFragment
 import com.margarita.filmviewer.ui.fragments.ErrorFragment
-import kotlinx.android.synthetic.main.fragment_list.*
-import kotlinx.android.synthetic.main.progress_bar.*
+import com.margarita.filmviewer.ui.fragments.MoviesFragment
 
-class MainActivity : AppCompatActivity(), MoviesView {
-
-    /**
-     * Adapter for RecyclerView
-     */
-    private val adapter: MovieAdapter by lazy {
-        MovieAdapter(object: MovieAdapter.OnMovieClickListener {
-            override fun onMovieClick(movie: Movie) {
-                container.showSnackBar(movie.title!!, Snackbar.LENGTH_SHORT)
-            }
-
-            override fun like(movie: Movie) {
-                //TODO
-            }
-        })
-    }
+class MainActivity : AppCompatActivity(), MoviesFragment.OnContentErrorListener {
 
     /**
-     * Presenter of movies
+     * All fragments
      */
-    private val presenter = MoviesPresenter(this)
+    private val moviesFragment by lazy { MoviesFragment() }
+    private val errorFragment by lazy { ErrorFragment() }
+    private val emptySearchFragment by lazy { EmptySearchFragment() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        /*
-        rvList.layoutManager = LinearLayoutManager(this)
-        rvList.adapter = adapter
-
-        swipeContainer.setColorSchemeResources(R.color.colorAccent)
-        swipeContainer.setOnRefreshListener { presenter.loadRefresh() }
-        */
-
         setupSearchView()
-        presenter.loadStart()
+        setFragment(moviesFragment)
     }
 
     /**
@@ -73,57 +43,12 @@ class MainActivity : AppCompatActivity(), MoviesView {
      * Function which implements the fragment replacement
      * @param fragment New fragment which will be shown
      */
-    private fun replaceFragment(fragment: Fragment) {
+    private fun setFragment(fragment: Fragment) {
         val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.add(R.id.container, fragment).commit()
+        fragmentTransaction.replace(R.id.container, fragment).commit()
     }
 
-    //region Loading content
-    override fun showLoadingContent() {
-        progressBar.visibility = View.VISIBLE
-    }
+    override fun showLoadingError(): Unit = setFragment(errorFragment)
 
-    override fun hideLoadingContent() {
-        progressBar.visibility = View.GONE
-    }
-
-    override fun showLoadingError(): Unit = replaceFragment(ErrorFragment())
-
-    override fun setMovies(movies: List<Movie>): Unit = adapter.setMovies(movies)
-    //endregion
-
-    //region Loading next or refreshing
-    override fun showLoadingNextContent() {
-        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun hideLoadingNextContent() {
-        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun addMovies(movies: List<Movie>): Unit = adapter.addMovies(movies)
-
-    override fun hideRefreshing() {
-        swipeContainer.isRefreshing = false
-    }
-
-    override fun showRefreshingError(messageRes: Int): Unit
-            = container.showSnackBar(messageRes)
-    //endregion
-
-    //region Search
-    override fun showSearchProgress() {
-        progressSearch.visibility = View.VISIBLE
-    }
-
-    override fun hideSearchProgress() {
-        progressSearch.visibility = View.VISIBLE
-    }
-
-    override fun showSearchError(): Unit = replaceFragment(EmptySearchFragment())
-
-    override fun setSearchResult(movies: List<Movie>) {
-        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-    //endregion
+    override fun showSearchError(): Unit = setFragment(emptySearchFragment)
  }
